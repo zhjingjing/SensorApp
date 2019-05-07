ca658d0963df21011612a952bfddb66b5a285645
zj <zh937613@gmail.com>
2019-05-07 10:40:46 +0800

    创建项目

---
+++ app/src/main/java/com/zh/sensor/MainActivity.java
@@ -0,0 +1,13 @@
+package com.zh.sensor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
